e8cae9f5db15251ffbb5d73e8aac2ce7ba79e337
dewanhasan99 <g00410156@atu.ie>
2024-12-27 18:53:58 +0000

    First Commit

---
+++ src/main/java/ie/atu/registrationm2/RegistrationM2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.registrationm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationM2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationM2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/registrationm2/RegistrationM2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.registrationm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationM2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
