a1d9ae41b83c68cab52b1a39a4f019f917b8def6
dewanhasan99 <g00410156@atu.ie>
2024-12-28 19:21:57 +0000

    Added connections with the first and second microservices with the ability to add and confirm courses.

--- src/main/java/ie/atu/registrationm2/RegistrationController.java
+++ src/main/java/ie/atu/registrationm2/RegistrationController.java
@@ -1,6 +1,7 @@
 package ie.atu.registrationm2;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class RegistrationController {
@@ -22,4 +24,26 @@ public class RegistrationController {
             + " Registered succesfully");
         return response;
     }
+
+
+    @PostMapping("/register")
+    public Map<String, String> registerStudent(@RequestBody StudentCourseRequest studentCourseRequest){
+        System.out.println("Student Details with Selected Courses: " + studentCourseRequest.getStudentDetails() + " || "
+        + studentCourseRequest.getCourseList());
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "accepted");
+        response.put("message", "Student: " + studentCourseRequest.getStudentDetails().getFirstname() + " " +
+                studentCourseRequest.getStudentDetails().getLastname() + " || Courses: " +
+                studentCourseRequest.getCourseList() + " has been Registered");
+        return response;
+    }
+
+    /*@PostMapping("/register")
+    public ResponseEntity<String> registerStudent(@RequestBody StudentCourseRequest studentCourseRequest){
+        String studentName = studentCourseRequest.getStudentDetails().getFirstname() + " " + studentCourseRequest.getStudentDetails().getLastname();
+        String courses = studentCourseRequest.getCourseList().stream().map(Course::getCourseDetails).collect(Collectors.joining(", "));
+
+        String message = "Registered Student Name: " + studentName + ", Selected Courses" + courses;
+        return ResponseEntity.ok(message);
+    }*/
 }
